0b7177942bd4d542d4f3c1fbfcbdf0052812f0ef
nitika vaish <vaishnitika95@gmail.com>
2025-09-13 01:17:32 +0530

    Updated removeOccurrences.java

--- firstprogram/src/com/apnaCollege/Recursion/removeOccurrences.java
+++ firstprogram/src/com/apnaCollege/Recursion/removeOccurrences.java
@@ -1,4 +1,34 @@
 package com.apnaCollege.Recursion;
 
 public class removeOccurrences {
+
+    // approach 1
+    public static String removeAlla(String s, int idx){
+        if(idx==s.length()) return "";
+        String smallAns=removeAlla(s,idx+1);
+        char currChar=s.charAt(idx);
+        if(currChar=='a'){
+            return smallAns;
+        }else{
+            return currChar+smallAns;
+        }
+    }
+
+    //approach 2
+    public static String removeAlla2(String s){
+        if(s.length()==0) return "";
+        String smallAns=removeAlla2(s.substring(1));
+        char currChar=s.charAt(0);
+        if(currChar=='a'){
+            return smallAns;
+        }else{
+            return currChar+smallAns;
+        }
+    }
+    public static void main(String[] args) {
+        String s= "abacx";
+        System.out.println(removeAlla(s,0));
+        System.out.println(removeAlla2(s));
+
+    }
 }
